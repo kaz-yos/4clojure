edd99067933344157d5af5c635ce7f755e5dbd48
kaz-yos <kazky@mac.com>
2014-05-06 18:59:30 -0400

    pronounciation q110

--- 4clojure.clj
+++ 4clojure.clj
@@ -1572,3 +1572,60 @@
 (= [21 6 1] ((__ + max min) 2 3 5 1 6 4))
 (= ["HELLO" 5] ((__ #(.toUpperCase %) count) "hello"))
 (= [2 6 4] ((__ :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
+
+
+
+
+;; 4Clojure Question 110
+;;
+;; <p>Write a function that returns a lazy sequence of "pronunciations" of a sequence of numbers. A pronunciation of each element in the sequence consists of the number of repeating identical numbers and the number itself. For example, <code>[1 1]</code> is pronounced as <code>[2 1]</code> ("two ones"), which in turn is pronounced as <code>[1 2 1 1]</code> ("one two, one one").</p><p>Your function should accept an initial sequence of numbers, and return an infinite lazy sequence of pronunciations, each element being a pronunciation of the previous element.</p>
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+;; function to get the next one from the seed collection
+(defn __ [coll]
+  (partition-by identity coll))
+
+(defn __ [coll]
+  (map count (partition-by identity coll)))
+
+(defn __ [coll]
+  (let [partitioned (partition-by identity coll)
+        nums (mapcat distinct partitioned)
+        counts (map count partitioned)]
+    (interleave counts nums)
+    ))
+
+(defn __ [coll]
+  (rest (iterate
+         ;; fun to iterate
+         (fn [coll]
+           (let [partitioned (partition-by identity coll)
+                 nums (mapcat distinct partitioned)
+                 counts (map count partitioned)]
+             ;;
+             (interleave counts nums)
+             ))
+         ;; coll to iterate
+         coll)))
+
+(defn __ [coll]
+  (rest (iterate
+         ;; fun to iterate
+         (fn [coll]
+           (let [partitioned (partition-by identity coll)]
+             ;;
+             (interleave (map count partitioned) (mapcat distinct partitioned))
+             ))
+         ;; coll to iterate
+         coll)))
+
+
+(= [[1 1] [2 1] [1 2 1 1]] (take 3 (__ [1])))
+
+(= [3 1 2 4] (first (__ [1 1 1 4 4])))
+
+(= [1 1 1 3 2 1 3 2 1 1] (nth (__ [1]) 6))
+
+(= 338 (count (nth (__ [3 2]) 15)))
+
