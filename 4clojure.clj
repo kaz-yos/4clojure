00c7989c1aa7a290f5e9cfe48980d6c4592e5885
kaz-yos <kazky@mac.com>
2014-05-20 18:22:10 -0400

    pairwise disjoint

--- 4clojure.clj
+++ 4clojure.clj
@@ -2080,3 +2080,72 @@
 (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
           [2 [3 nil [4 [6 nil nil] nil]] nil]])
    false)
+
+
+
+
+;;; 4Clojure Question 153
+;;
+;; Given a set of sets, create a function which returns <code>true</code> 
+;;
+;; if no two of those sets have any elements in common<sup>1</sup> and <code>false</code> otherwise. 
+;;
+;; Some of the test cases are a bit tricky, so pay a little more attention to them. 
+;; <sup>1</sup>Such sets are usually called <i>pairwise disjoint</i> or <i>mutually disjoint</i>.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+
+(defn __ [a-set]
+  (->> (for [x a-set
+             y (disj a-set x)]
+         (clojure.set/intersection x y)) ; all possible pairwise intersection
+       ;; true if all empty
+       (every? empty?,  )))
+
+
+;; actually can just open it up and check element-wise
+(apply distinct? (apply concat #{#{:a :b :c :d :e}
+                                  #{:a :b :c :d}
+                                  #{:a :b :c}
+                                  #{:a :b}
+                                  #{:a}}))
+
+(= (__ #{#{\U} #{\s} #{\e \R \E} #{\P \L} #{\.}})
+   true)
+(= (__ #{#{:a :b :c :d :e}
+         #{:a :b :c :d}
+         #{:a :b :c}
+         #{:a :b}
+         #{:a}})
+   false)
+(= (__ #{#{[1 2 3] [4 5]}
+         #{[1 2] [3 4 5]}
+         #{[1] [2] 3 4 5}
+         #{1 2 [3 4] [5]}})
+   true)
+(= (__ #{#{'a 'b}
+         #{'c 'd 'e}
+         #{'f 'g 'h 'i}
+         #{''a ''c ''f}})
+   true)
+(= (__ #{#{'(:x :y :z) '(:x :y) '(:z) '()}
+         #{#{:x :y :z} #{:x :y} #{:z} #{}}
+         #{'[:x :y :z] [:x :y] [:z] [] {}}})
+   false)
+(= (__ #{#{(= "true") false}
+         #{:yes :no}
+         #{(class 1) 0}
+         #{(symbol "true") 'false}
+         #{(keyword "yes") ::no}
+         #{(class '1) (int \0)}})
+   false)
+(= (__ #{#{distinct?}
+         #{#(-> %) #(-> %)}
+         #{#(-> %) #(-> %) #(-> %)}
+         #{#(-> %) #(-> %) #(-> %)}})
+   true)
+(= (__ #{#{(#(-> *)) + (quote mapcat) #_ nil}
+         #{'+ '* mapcat (comment mapcat)}
+         #{(do) set contains? nil?}
+         #{, , , #_, , empty?}})
+   false)
