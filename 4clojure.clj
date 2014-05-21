60aedbe080b3d20924639c4e3c63be12d78ae098
kaz-yos <kazky@mac.com>
2014-05-21 07:34:46 -0400

    Q85 powerset added

--- 4clojure.clj
+++ 4clojure.clj
@@ -2465,3 +2465,26 @@
    {1 7, 2 10, 3 15})
 (= (__ concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
    {:a [3 4 5], :b [6 7], :c [8 9]})
+
+
+
+;;; 4Clojure Question 85
+;;
+;; Write a function which generates the <a href="http://en.wikipedia.org/wiki/Power_set">power set</a> of a given set.  The power set of a set x is the set of all subsets of x, including the empty set and x itself.
+;;
+;; Use M-x 4clojure-check-answers when you're done!
+;;
+;; signature: set -> set of sets
+;; purpose: powerset of a given set
+;; stub:
+;; (defn __ [a-set]
+;;   #{})
+;;
+(defn __ [a-set]
+  )
+
+(= (__ #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})
+(= (__ #{}) #{#{}})
+(= (__ #{1 2 3})
+   #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
+(= (count (__ (into #{} (range 10)))) 1024)
